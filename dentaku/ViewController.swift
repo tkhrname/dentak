8dbb100113380542f0c50c4ce325adc68faab7bd
渡邊丈洋 <takehiro.watanabe@d2cdot.co.jp>
2020-07-02 17:24:56 +0900

    Initial Commit

---
+++ dentaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dentaku
+//
+//  Created by 渡邊 丈洋 on 2020/07/02.
+//  Copyright © 2020 tkhrname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
